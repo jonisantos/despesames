e38f227550b94bc01e6c10b4626d32a6c7568f66
JonilsonNascimento <jonnyzerobravo@gmail.com>
2019-07-11 12:56:25 -0400

    Implementando autenticacao basica

--- src/main/java/com/ifmt/geri/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/ifmt/geri/repository/lancamento/LancamentoRepositoryImpl.java
@@ -1,4 +1,5 @@
-package com.ifmt.geri.repository.filter;
+package com.ifmt.geri.repository.lancamento;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +12,14 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.ifmt.geri.model.Lancamento;
 import com.ifmt.geri.model.Lancamento_;
-import com.ifmt.geri.repository.lancamento.LancamentoRepositoryQuery;
+import com.ifmt.geri.repository.filter.LancamentoFilter;
 
 
 
@@ -25,7 +29,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 	private EntityManager manager;
 	
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 		Root<Lancamento> root = criteria.from(Lancamento.class);
@@ -34,7 +38,9 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		criteria.where(predicates);
 		
 		TypedQuery<Lancamento> query = manager.createQuery(criteria);
-		return query.getResultList();
+		adicionarRestricoesDePagincao(query, pageable);
+		
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
 
 	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
@@ -57,5 +63,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		}
 		
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+	
+	private void adicionarRestricoesDePagincao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistroPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPagina = paginaAtual * totalRegistroPorPagina;
+		
+		query.setFirstResult(primeiroRegistroDaPagina);
+		query.setMaxResults(totalRegistroPorPagina);
+	}
+	
+	private Long total(LancamentoFilter lancamentoFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		criteria.select(builder.count(root));
+		return manager.createQuery(criteria).getSingleResult();
+		
+		
 	}
 }
--- src/main/java/com/ifmt/geri/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/ifmt/geri/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,14 @@
 package com.ifmt.geri.repository.lancamento;
 
-import java.util.List;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.ifmt.geri.model.Lancamento;
 import com.ifmt.geri.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
